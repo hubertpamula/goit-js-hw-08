b51c64a59da38cf72955582ed160934f80085313
Hubert Pamuła <hubertpamula@gmail.com>
2023-07-13 22:30:38 +0200

    ad

--- dist/01-gallery.53390864.js
+++ dist/01-gallery.53390864.js
@@ -534,7 +534,7 @@ const lightbox = new (0, _simplelightboxDefault.default)(".gallery a", {
     captionType: "alt"
 });
 
-},{"./gallery-items.js":"9C7dK","simplelightbox":"9ydBq","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","simplelightbox/dist/simple-lightbox.min.css":"kaxSc"}],"9C7dK":[function(require,module,exports) {
+},{"./gallery-items.js":"9C7dK","simplelightbox":"9ydBq","simplelightbox/dist/simple-lightbox.min.css":"kaxSc","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"9C7dK":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "galleryItems", ()=>galleryItems);
